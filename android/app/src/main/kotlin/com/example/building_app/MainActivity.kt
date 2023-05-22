c1f3b3e9e6b1fbdfe5d84b5b84263c47f605f1a7
Triangle <halbert370@gmail.com>
2023-05-22 12:27:35 +0300

    .

--- android/app/src/main/kotlin/com/example/building_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/building_app/MainActivity.kt
@@ -9,12 +9,12 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
         }
+
+        super.onCreate(savedInstanceState)
     }
 }
