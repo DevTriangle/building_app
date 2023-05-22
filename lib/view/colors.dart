663cb1b6a741700617469c188f43132e31b7c301
DevTriangle <halbert370@gmail.com>
2023-05-22 14:26:03 +0300

    ß

--- lib/view/colors.dart
+++ lib/view/colors.dart
@@ -4,5 +4,5 @@ class AppColors {
   static Color primaryColor = const Color(0xFF3974FF);
   static Color cardColor = const Color.fromARGB(255, 240, 240, 240);
   static Color backgroundColor = const Color.fromARGB(255, 255, 255, 255);
-  static Color hintColor = const Color(0xFFb7b5ab);
+  static Color hintColor = const Color(0xFF6E6E6E);
 }
