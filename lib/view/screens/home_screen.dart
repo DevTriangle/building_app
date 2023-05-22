1f686ff431fd05cbeb275855abf93d23ade20b8c
Triangle <halbert370@gmail.com>
2023-05-22 08:26:20 +0300

    .

---
+++ lib/view/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
