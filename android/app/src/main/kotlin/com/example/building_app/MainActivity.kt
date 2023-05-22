80c847f86f8085b43978099c67abed3160eb2845
DevTriangle <halbert370@gmail.com>
2023-05-22 09:20:06 +0300

    ß

--- android/app/src/main/kotlin/com/example/building_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/building_app/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.building_app
 
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
+        }
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:building_app/view/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ,
+      home: HomeScreen(),
     );
   }
 }
